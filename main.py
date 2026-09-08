d73938c01e1f5c4a579d4c1bc2fde554953962e8
Fahad Khan <107381319+nullscaped@users.noreply.github.com>
2026-09-08 14:46:55 -0400

    Add basic search and result ranking

--- main.py
+++ main.py
@@ -2,4 +2,19 @@ from search_engine import SearchEngine
 
 engine = SearchEngine()
 
-print(engine.documents, engine.index)
+engine.add_document(
+    "doc1.txt",
+    "Python is a programming language used for backend development."
+)
+
+engine.add_document(
+    "doc2.txt",
+    "Chess is a strategy game with multiplayer servers."
+)
+
+engine.add_document(
+    "doc3.txt",
+    "Node.js can be used to create multiplayer applications."
+)
+
+print(engine.search("multiplayer servers"))
--- search_engine.py
+++ search_engine.py
@@ -1,4 +1,41 @@
+import re
+
 class SearchEngine:
     def __init__(self):
         self.documents = {}
         self.index = {}
+
+    def search(self, query):
+        words = re.findall(r"\b\w+\b", query.lower())
+
+        scores = {}
+
+        for word in words:
+            if word in self.index:
+                for document in self.index[word]:
+                    if document not in scores:
+                        scores[document] = 0
+
+                    scores[document] += 1
+
+        ranked_results = sorted(
+            scores.items(),
+            key=lambda item: item[1],
+            reverse=True
+        )
+
+        return ranked_results
+
+        return list(results)
+
+    def add_document(self, name, text):
+        self.documents[name] = text
+
+        words = re.findall(r"\b\w+\b", text.lower())
+
+        for word in words:
+            if word not in self.index:
+                self.index[word] = []
+
+            if name not in self.index[word]:
+                self.index[word].append(name)
